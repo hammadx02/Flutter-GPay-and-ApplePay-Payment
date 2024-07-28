abc960353d2f6aad47e77a3de11e9f813a0f9362
hammadx02 <hammadaali02@gmail.com>
2024-07-28 22:32:03 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gpay_and_applepay_integration/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'GPay and ApplePay',
       theme: ThemeData(
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: 
+      home: const HomeScreen(),
     );
   }
 }
