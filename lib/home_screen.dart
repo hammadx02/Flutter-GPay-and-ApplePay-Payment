178f490578324eda83579c391d3ac186ad49af21
hammadx02 <hammadaali02@gmail.com>
2024-07-28 22:38:30 +0500

    apple pay integartion

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_gpay_and_applepay_integration/payment_config.dart';
+import 'package:pay/pay.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +12,18 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String os = Platform.operatingSystem;
+
+  var applePayButton = ApplePayButton(
+    paymentConfiguration: PaymentConfiguration.fromJsonString(defaultApplePay),
+    paymentItems: const [
+      PaymentItem(
+        amount: '250',
+        label: 'Iphone 13 Pro Max',
+        status: PaymentItemStatus.final_price,
+      ),
+    ],
+  );
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
